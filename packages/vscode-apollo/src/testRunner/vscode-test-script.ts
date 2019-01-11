006b6b73032e4bf2d9ee51681c5028da2a944b60
Trevor Scheer <trevor@apollographql.com>
2019-01-10 17:26:36 -0800

    Add try/catch to dig into error

--- packages/vscode-apollo/src/testRunner/vscode-test-script.ts
+++ packages/vscode-apollo/src/testRunner/vscode-test-script.ts
@@ -1,18 +1,26 @@
 import { resolve } from "path";
 import { spawnSync } from "child_process";
 
-spawnSync(
-  `node ${resolve(process.cwd(), "..", "..")}/node_modules/vscode/bin/test`,
-  {
-    stdio: "inherit",
-    shell: true,
-    env: {
-      CODE_TESTS_PATH: `${process.cwd()}/lib/testRunner`,
-      // Note:
-      // Extensions that boot up and throw warnings on the host machine will cause tests to fail,
-      // so running with --disable-extensions is a must. This env variable in particular is open
-      // to injection in this manner, so I did it with a sad face.
-      CODE_TESTS_WORKSPACE: "--disable-extensions"
+try {
+  spawnSync(
+    `node ${resolve(process.cwd(), "..", "..")}/node_modules/vscode/bin/test`,
+    {
+      stdio: "inherit",
+      shell: true,
+      env: {
+        CODE_TESTS_PATH: `${process.cwd()}/lib/testRunner`,
+        // Note:
+        // Extensions that boot up and throw warnings on the host machine will cause tests to fail,
+        // so running with --disable-extensions is a must. This env variable in particular is open
+        // to injection in this manner, so I did it with a sad face.
+        CODE_TESTS_WORKSPACE: "--disable-extensions"
+      }
     }
-  }
-);
+  );
+} catch (error) {
+  console.log(`Status: ${error.status})`);
+  console.log(`Message: ${error.message})`);
+  console.log(`Stderr: ${error.stderr.toString()}`);
+
+  throw error;
+}
