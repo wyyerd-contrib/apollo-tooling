43e019a130db2f93495c0d5f48cc68e0bf339ba2
Trevor Scheer <trevor@apollographql.com>
2019-01-14 15:31:53 -0800

    Add verbose flag for debugging

--- packages/vscode-apollo/src/testRunner/vscode-test-script.ts
+++ packages/vscode-apollo/src/testRunner/vscode-test-script.ts
@@ -20,7 +20,7 @@ const buf = spawnSync(
       // Extensions that boot up and throw warnings on the host machine will cause tests to fail,
       // so running with --disable-extensions is a must. This env variable in particular is open
       // to injection in this manner, so I did it with a sad face.
-      CODE_TESTS_WORKSPACE: "--disable-extensions"
+      CODE_TESTS_WORKSPACE: "--verbose"
     }
   }
 );
