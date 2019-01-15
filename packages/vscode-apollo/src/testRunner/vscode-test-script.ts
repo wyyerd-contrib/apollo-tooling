f63d756ee5c829aba48ccea9b08d8420e21d79b1
Trevor Scheer <trevor@apollographql.com>
2019-01-14 16:44:26 -0800

    more logs

--- packages/vscode-apollo/src/testRunner/vscode-test-script.ts
+++ packages/vscode-apollo/src/testRunner/vscode-test-script.ts
@@ -15,17 +15,19 @@ const buf = spawnSync(
   {
     shell: true,
     env: {
-      CODE_TESTS_PATH: `${process.cwd()}/lib/testRunner`,
+      CODE_TESTS_PATH: `${process.cwd()}/lib/testRunner`
       // Note:
       // Extensions that boot up and throw warnings on the host machine will cause tests to fail,
       // so running with --disable-extensions is a must. This env variable in particular is open
       // to injection in this manner, so I did it with a sad face.
-      CODE_TESTS_WORKSPACE: "--verbose"
+      // CODE_TESTS_WORKSPACE: "--verbose"
     }
   }
 );
 
 console.error(buf.stderr.toString());
 console.log(buf.stdout.toString());
-
+console.log(`status: ${buf.status}`);
+console.log(`signal: ${buf.signal}`);
+console.log(`error: ${buf.error}`);
 console.log("Test runner finished");
